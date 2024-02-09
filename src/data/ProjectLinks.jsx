21d413f7cda67053fbba9a9e7bb0f536c51142a7
Sumon <sumonkhan1081@gmail.com>
2024-02-10 04:51:45 +0530

    new project added

--- src/data/ProjectLinks.jsx
+++ src/data/ProjectLinks.jsx
@@ -2,31 +2,39 @@ import IMG1 from "../assets/Expense-tracker.png";
 import IMG2 from "../assets/Hotel-odisej.jpg";
 import IMG3 from "../assets/Shopping-App.png"
 import IMG4 from "../assets/EJ-Studio.png"
+import IMG5 from "../assets/StudyNotion.png"
 
 export const data = [
     {
         id:1,
+        img:IMG5,
+        title:"MERN Stack | (StudyNotion - EdTech)",
+        github:"https://github.com/Sumonnn/StudyNotion-EdTech",
+        demo:"https://studynotion-frontend.vercel.app/",
+    },
+    {
+        id:2,
         img:IMG3,
         title:"React | Redux (Shopping-App)",
         github:"https://github.com/Sumonnn/shop-app-redux",
         demo:"https://my-online-shopping-app.netlify.app/",
     },
     {
-        id:2,
+        id:3,
         img:IMG1,
         title:"EJS | CSS | Node.js (Expense-Tracker)",
         github:"https://github.com/Sumonnn/Expense-Tracker",
         demo:"https://expensetracker-1ugl.onrender.com/",
     },
     {
-        id:3,
+        id:4,
         img:IMG2,
         title:"HTML | CSS | JavaScript (Hotel-Odisej)",
         github:"https://github.com/Sumonnn/Hotel-Odisej",
         demo:"https://sumonnn.github.io/Hotel-Odisej/",
     },
     {
-        id:4,
+        id:5,
         img:IMG4,
         title:"HTML | CSS | JavaScript (EJ-Studio)",
         github:"https://github.com/Sumonnn/EJ-STUDIO",
