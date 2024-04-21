dbc08b52711322dd2ace75a08c711d5d58390134
Sumon <sumonkhan1081@gmail.com>
2024-04-21 23:57:49 +0530

    new project added

--- src/data/ProjectLinks.jsx
+++ src/data/ProjectLinks.jsx
@@ -3,6 +3,7 @@ import IMG2 from "../assets/Hotel-odisej.jpg";
 import IMG3 from "../assets/Shopping-App.png"
 import IMG4 from "../assets/EJ-Studio.png"
 import IMG5 from "../assets/StudyNotion.png"
+import IMG6 from "../assets/Movie-App.png"
 
 export const data = [
     {
@@ -14,27 +15,34 @@ export const data = [
     },
     {
         id:2,
+        img:IMG6,
+        title:"React | Redux (Movie-Search-App)",
+        github:"https://github.com/Sumonnn/Movie-App-React",
+        demo:"https://sumon-movie-search-app.vercel.app/",
+    },
+    {
+        id:3,
         img:IMG3,
         title:"React | Redux (Shopping-App)",
         github:"https://github.com/Sumonnn/shop-app-redux",
         demo:"https://my-online-shopping-app.netlify.app/",
     },
     {
-        id:3,
+        id:4,
         img:IMG1,
         title:"EJS | CSS | Node.js (Expense-Tracker)",
         github:"https://github.com/Sumonnn/Expense-Tracker",
         demo:"https://expensetracker-1ugl.onrender.com/",
     },
     {
-        id:4,
+        id:5,
         img:IMG2,
         title:"HTML | CSS | JavaScript (Hotel-Odisej)",
         github:"https://github.com/Sumonnn/Hotel-Odisej",
         demo:"https://sumonnn.github.io/Hotel-Odisej/",
     },
     {
-        id:5,
+        id:6,
         img:IMG4,
         title:"HTML | CSS | JavaScript (EJ-Studio)",
         github:"https://github.com/Sumonnn/EJ-STUDIO",
