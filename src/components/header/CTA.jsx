63852a7b63cdcb83618b242784b6a42f422bac3d
Sumon <sumonkhan1081@gmail.com>
2024-04-26 00:19:13 +0530

    done

--- src/components/header/CTA.jsx
+++ src/components/header/CTA.jsx
@@ -3,7 +3,7 @@ import React from "react";
 const CTA = () => {
   return (
     <div className="cta">
-      <a href="https://drive.google.com/file/d/1yJDvAIacXIUKggaVEd77LFxrBuGYmBMm/view?usp=sharing" target="_blank" className="btn">
+      <a href="https://drive.google.com/file/d/14hp2OnK6OdMemj_aUdgAOKBhKffaIPan/view?usp=sharing" target="_blank" className="btn">
         Resume
       </a>
       <a href="#contact" className="btn btn-primary">
